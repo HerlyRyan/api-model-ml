b6e232a420dab5a21de67067c23e518f84a2ae8f
HerlyRyan <herlyryanth@gmail.com>
2023-12-22 08:32:51 +0800

    First upload

---
+++ README.md
@@ -0,0 +1,64 @@
+# ML API Template 🚀
+
+Hello everyone! In this repository, I (Kaenova, a mentor in Bangkit 2023 program) will provide you with a head start on creating an ML API. Please read every line and comment carefully.
+
+I have included code examples for both text-based input and image-based input APIs. To run this server, make sure to install all the required libraries listed in `requirements.txt` by executing the command `pip install -r requirements.txt`. Then, use the command `python recommendation.py` to run the server.
+
+## Machine Learning Setup
+
+Please prepare your model in either the `.h5` or saved model format and data_rating3.csv. Place your model in the same folder as the `recommendation.py` file. You will load your model in the code provided below. There are two options available: one for predict rating and another for get recommendation. You need to complete either the `def predict_rating` or `def get_recommendation` functions in `recommendation.py` based on your model's input type.
+
+## Cloud Computing
+
+You can check the endpoints used for the machine learning models in this API. The available endpoints are `/predict_rating` for get rating from user_id and product_name input and `/get_recommendation` for get recommendation from user_id.
+
+For the `/predict_rating` endpoint, you need to send a JSON payload with the following structure:
+```json
+{
+  "user_id": 1,
+  "product_name": "kuda"
+}
+```
+note: input "user_id" and "product_name" value based on data_rating3.csv
+
+For the `/get_recommendation` endpoint, you need to send a JSON payload with the following structure:
+```json
+{
+  "user_id": 1
+}
+```
+You can view the API documentation by accessing the `/docs` endpoint after running the server. Additionally, a Dockerfile is provided to facilitate modification and container image creation. By default, the server runs on port 8080, but you can customize the port by injecting the `PORT` environment variable.
+
+## Usage
+
+To get started, follow these steps:
+
+1. Clone the repository:
+```sh
+git clone https://github.com/your-username/your-repository.git
+```
+
+2. Install the required libraries:
+```sh
+pip install -r requirements.txt
+```
+
+3. Prepare your machine learning model:
+- If you have an `.h5` model file, place it in the same folder as `recommendation.py`.
+- If you have a saved model format, place it in a folder named `my_model_folder` in the same directory as `recommendation.py`.
+
+4. Read the `recommendation.py` file carefully
+
+5. Complete the `predict_rating` or `get_recommendation` function in `recommendation.py`
+
+6. Run the server:
+```sh
+python recommendation.py
+```
+
+---
+
+## One More Thing
+
+One thing i should mention. Many people still asking **Who should deploy the model? Is it CC or is it ML?**
+> Both parties need to collaborate. Cloud Computing (CC) may not be familiar with the contents of your .h5 file, and Machine Learning (ML) may not be familiar with HTTP POST and GET requests. Therefore, it is the responsibility of both parties to deploy the model.
---
+++ recommendation.py
@@ -0,0 +1,112 @@
+import os
+import uvicorn
+import traceback
+import tensorflow as tf
+import pandas as pd
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from sklearn.neighbors import NearestNeighbors
+
+model_path = "./recommendation_model.h5"
+model = tf.keras.models.load_model(model_path)
+
+# Load the dataset
+data = pd.read_csv("data_rating3.csv") 
+
+# Remove duplicates from the dataset
+data_no_duplicates = data.drop_duplicates(subset=['user_id', 'product'])
+
+# Create a FastAPI app
+app = FastAPI()
+
+# Define the request model for prediction with product_name
+class RatingPredictionRequest(BaseModel):
+    user_id: int
+    product_name: str
+
+# Define the response model for prediction
+class RatingPredictionResponse(BaseModel):
+    prediction: float
+
+# Define the request model for recommendation
+class RatingRecommendationRequest(BaseModel):
+    user_id: int
+
+# Define the response model for recommendation with product names and ratings
+class RatingRecommendationResponseWithNamesAndRatings(BaseModel):
+    recommendations: dict
+    average_rating: float
+
+# Define the endpoint for prediction
+@app.post("/predict_rating", response_model=RatingPredictionResponse)
+def predict_rating(request: RatingPredictionRequest):
+    try:
+        # Find the product_id based on product_name
+        product_rating = data_no_duplicates[(data_no_duplicates['product'] == request.product_name) & (data_no_duplicates['user_id'] == request.user_id)]['rating'].values
+        if len(product_rating) == 0:
+            raise HTTPException(status_code=404, detail="Product not found for the given user")
+        result = product_rating.mean()
+        # Return the prediction
+        return {"prediction": float(result)}
+
+    except HTTPException as e:
+        raise  # Reraise HTTPException to maintain status code and detail
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+# Define the endpoint for recommendation with product names and ratings
+@app.post("/get_recommendation", response_model=RatingRecommendationResponseWithNamesAndRatings)
+def get_recommendation(request: RatingRecommendationRequest):
+    try:
+        # Aggregate ratings for duplicate entries (taking the average)
+        user_ratings = data.groupby(['user_id', 'product'], as_index=False)['rating'].mean()
+
+        # Filter data based on the user's rating
+        user_ratings_filtered = user_ratings[user_ratings['user_id'] == request.user_id][['user_id', 'product', 'rating']]
+
+        # Check if there are enough samples for the specified user
+        if len(user_ratings_filtered) >= 5:
+            # Create a pivot table for user-item matrix
+            user_item_matrix = user_ratings_filtered.pivot(index='user_id', columns='product', values='rating').fillna(0)
+
+            # Fit Nearest Neighbors model
+            knn_model = NearestNeighbors(metric='cosine', algorithm='brute')
+
+            # Check if there are enough samples for fitting the model
+            if len(user_item_matrix) >= 2:
+                knn_model.fit(user_item_matrix)
+
+                # Find k-neighbors for the given user
+                _, indices = knn_model.kneighbors(user_item_matrix.loc[[request.user_id]], n_neighbors=5)
+
+                # Get recommended products with their ratings
+                recommended_products_data = user_ratings.loc[user_ratings['user_id'].isin(indices[0])][['product', 'rating']].drop_duplicates()
+
+                # Create a dictionary of recommended products with their ratings
+                recommended_products_dict = {}
+                for _, row in recommended_products_data.iterrows():
+                    recommended_products_dict[row["product"]] = float(row["rating"])
+
+                # Calculate the average rating of recommended products
+                if recommended_products_data.empty:
+                    average_rating = 0.0
+                else:
+                    average_rating = recommended_products_data['rating'].mean()
+
+                return {"recommendations": recommended_products_dict, "average_rating": float(average_rating)}
+            else:
+                return {"recommendations": {}, "average_rating": 0.0}
+        else:
+            return {"recommendations": {}, "average_rating": 0.0}
+
+    except HTTPException as e:
+        raise  # Reraise HTTPException to maintain status code and detail
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+# Run the FastAPI app
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
